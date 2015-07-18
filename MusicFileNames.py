86c99c84878b9cb2d0caa47393d010b709fdf759
DistinguishedBagel <distinguishedbagel@gmail.com>
2015-07-18 22:22:19 +0100

    Update MusicFileNames.py

--- MusicFileNames.py
+++ MusicFileNames.py
@@ -1,10 +1,20 @@
 import os
+import eyed3
 
 f = open("MusicFiles1.txt", "a")
-for root, dirs, files in os.walk("C:/Users/Khalil/Music/"):
+for root, dirs, files in os.walk("/home/khalil/Music/"):
     for file in files:
         if file.endswith(".mp3"):
-            print(file)
-            s = str(file)
-            f.write(s+"\n")
+            # load id3 tag object into variable
+            mfile = eyed3.load(os.path.join(root, file))
+            if mfile.tag.title is None:
+                print("No tag for " + file + ".")
+            else:
+                # print id3 tag into to std out
+                #print (mfile.tag.title)
+                # load the id3 tags you want into a string
+                line = mfile.tag.title
+                #s = str(mfile)
+                # write out to file
+                f.write(line+"\n")
 f.close()
